07ab77337de26336891b649b80e64d36b6c0f04c
goyslee <goyslee@gmail.com>
2023-10-01 17:40:03 +0100

    first deployment

---
+++ scripts.js
@@ -0,0 +1,48 @@
+// Display the current year in the footer
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const projectTitles = document.querySelectorAll('.project-title');
+
+    projectTitles.forEach(title => {
+        title.addEventListener('click', function() {
+            // The image should be inside an anchor that's a direct sibling of our title.
+            const img = this.nextElementSibling.querySelector('img');
+            if (img) {
+                img.classList.toggle('hidden');
+            }
+        });
+    });
+});
+
+function updateSecondsSince() {
+    // The start date (01/04/2016 midnight)
+    const startDate = new Date("2016-04-01T00:00:00Z");
+
+    // Current date and time
+    const now = new Date();
+
+    // Difference in milliseconds
+    const diffMilliseconds = now - startDate;
+
+    // Convert to seconds
+    const diffSeconds = Math.floor(diffMilliseconds / 1000);
+
+    // Display in the about section
+    const counterElement = document.getElementById("seconds-counter");
+    counterElement.textContent = `I have been coding for ${diffSeconds} seconds.`;
+}
+
+// Call the function immediately
+updateSecondsSince();
+
+// Update every second
+setInterval(updateSecondsSince, 1000);
+
+
+
+
+
+
+
+
