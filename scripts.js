9e5ffa4dd2c8b83b1a3609296c0df6da43666eda
goyslee <goyslee@gmail.com>
2024-02-26 20:44:58 +0000

    animations more

--- scripts.js
+++ scripts.js
@@ -1,11 +1,44 @@
 // Display the current year in the footer
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
+document.addEventListener("DOMContentLoaded", (event) => {
+  const greetingElement = document.getElementById("greetings");
+  const greetingTexts = ["Hello... I am a FULLSTACK ENGINEER...", "Welcome to my Portfolio!"];
+  let currentTextIndex = 0;
+
+  function animateText() {
+    greetingElement.textContent = ""; // Clear previous text
+    const greetingText = greetingTexts[currentTextIndex];
+    let delay = 150;
+
+    for (let i = 0; i < greetingText.length; i++) {
+      const char = greetingText[i];
+      const span = document.createElement("span");
+
+      span.style.color = currentTextIndex === 0 ? "hsl(12, 90%, 63%)" : "white";
+      span.textContent = char === " " ? "\xa0" : char;
+      greetingElement.appendChild(span);
+
+      setTimeout(() => {
+        span.style.opacity = 1;
+      }, delay);
+
+      delay += 150;
+    }
+
+    currentTextIndex = (currentTextIndex + 1) % greetingTexts.length; // Cycle through texts
+  }
+
+  animateText(); // Start with the first text
+
+  // Call animateText again after a delay based on the first animation's duration
+  setTimeout(animateText, greetingTexts[0].length * 150 + 1000);
+});
+
 let fallDirection = 1;
 document.querySelectorAll('input[name="trailside"]').forEach((r) => {
   r.addEventListener("change", (e) => {
     fallDirection = parseInt(e.target.value);
-    //console.log(fallDirection);
   });
 });
 
@@ -163,18 +196,9 @@ const keywords = [
   "__debugInfo",
 ];
 
-function getRandomElement(arr) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
-}
-
-let x1 = 0,
-  y1 = 0;
-window.client;
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0),
-  dist_to_draw = 20,
-  delay = 2200,
-  fsize = [".6rem", ".8rem", ".2rem", "1.1rem"],
+  delay = 9000,
+  fsize = ["0.6rem", "0.8rem", "1.0rem", "1.2rem"],
   colors = [
     "hsl(12, 90%, 63%)",
     "hsl(12, 90%, 63%)",
@@ -185,58 +209,45 @@ const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeig
   ],
   rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
   selRand = (o) => o[rand(0, o.length - 1)],
-  distanceTo = (x1, y1, x2, y2) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)),
-  shouldDraw = (x, y) => {
-    const distance = distanceTo(x1, y1, x, y);
-    const shouldCreate = distance >= dist_to_draw && Math.random() > 0.7; // 30% chance to skip
-    return shouldCreate;
-  },
-  addStr = (x, y) => {
-    const str = document.createElement("div");
-    str.innerHTML = getRandomElement(keywords);
-    str.className = "star";
-    str.style.top = `${y + rand(-20, 20)}px`;
-    str.style.left = `${x}px`;
-    str.style.color = selRand(colors);
-    str.style.fontSize = selRand(fsize);
-    document.body.appendChild(str);
-    //console.log(rand(0, 3));
-    const fs = 10 + 5 * parseFloat(getComputedStyle(str).fontSize);
-    //console.log(vh, y, fs);
-    //console.log((y+fs)>vh?vh-y:fs);
-    str.animate(
-      {
-        translate: [
-          `${rand(-5, 5)}px ${(y + fs > vh ? vh - y : fs) * fallDirection * 0.3}px`,
-          `${rand(-20, 20)}px ${(y + fs > vh ? vh - y : fs) * fallDirection}px`,
-        ],
-        opacity: [1, 0],
-        transform: [
-          "rotateX(0) rotateY(0)",
-          `rotateX(${rand(1, 500)}deg) rotateY(${rand(1, 500)}deg)`,
-        ],
-      },
-      {
-        duration: delay,
-        fill: "forwards",
-        rangeStart: ["cover 0% ", "cover 40%"],
-        rangeEnd: ["cover 40% ", "cover 100%"],
-      },
-    );
-    //could add a animation terminate listener, but why add the additional load
-    setTimeout(() => {
-      str.remove();
-    }, delay);
-  };
+  getRandomElement = (arr) => arr[rand(0, arr.length - 1)];
 
-addEventListener("mousemove", (e) => {
-  const { clientX, clientY } = e;
-  if (shouldDraw(clientX, clientY)) {
-    addStr(clientX, clientY);
-    x1 = clientX;
-    y1 = clientY;
-  }
-});
+function addStr() {
+  const x = rand(0, window.innerWidth);
+  const y = rand(0, vh);
+  const str = document.createElement("div");
+  str.innerHTML = getRandomElement(keywords); // Now using a random keyword
+  str.className = "star";
+  str.style.top = `${y}px`;
+  str.style.left = `${x}px`;
+  str.style.color = selRand(colors);
+  str.style.fontSize = selRand(fsize);
+  document.body.appendChild(str);
+
+  str.animate(
+    {
+      translate: [
+        `${rand(-5, 5)}px ${(y > vh ? vh - y : y) * fallDirection * 0.3}px`,
+        `${rand(-20, 20)}px ${(y > vh ? vh - y : y) * fallDirection}px`,
+      ],
+      opacity: [1, 0],
+      transform: [
+        "rotateX(0) rotateY(0)",
+        `rotateX(${rand(1, 500)}deg) rotateY(${rand(1, 500)}deg)`,
+      ],
+    },
+    {
+      duration: delay,
+      fill: "forwards",
+    },
+  );
+
+  setTimeout(() => {
+    str.remove();
+  }, delay);
+}
+
+// Set up an interval to continuously add stars
+setInterval(addStr, 300); // Adjust the 300ms interval to your liking
 
 document.addEventListener("DOMContentLoaded", function () {
   const projectTitles = document.querySelectorAll(".project-title");
